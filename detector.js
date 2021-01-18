7a301b7187bd71ef5904d25f8b6c117c018358fe
Gacaes <77614712+Gacaes@users.noreply.github.com>
2021-01-18 15:41:59 +0000

    Update detector.js

--- detector.js
+++ detector.js
@@ -1,3 +1,12 @@
 var user_agent=navigator.userAgent.toLowerCase();
-console.log(user_agent)
-console.log("this is working")
+if (user_agent.search("iphone") > -1) {
+    alert('Thanks For Visiting On IPhone')
+}else if (user_agent.search("windows") > -1) {
+    alert('Thanks For Visiting On Windows')
+}else if (user_agent.search("android") > -1){
+    alert('Thanks For Visiting On Android')
+}else{
+    alert('Thanks For Visiting On Other Device')
+    alert(user_agent)
+}
+console.log(user_agent);
